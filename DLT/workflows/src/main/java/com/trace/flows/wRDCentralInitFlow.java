b3fb9a2cc9ff11431c43e72dab4c315794679d68
reevald <mochammadgalangrivaldo@gmail.com>
2025-08-03 01:46:21 +0700

    MVP: phase-2 | wRDCentralInitFlow

--- DLT/workflows/src/main/java/com/trace/flows/wRDCentralInitFlow.java
+++ DLT/workflows/src/main/java/com/trace/flows/wRDCentralInitFlow.java
@@ -4,39 +4,45 @@ import co.paralleluniverse.fibers.Suspendable;
 import com.trace.states.wRDAccountState;
 import com.trace.contracts.wRDContract;
 import net.corda.core.contracts.Amount;
+import net.corda.core.crypto.SecureHash;
 import net.corda.core.flows.*;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.identity.Party;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.transactions.TransactionBuilder;
 import net.corda.core.utilities.ProgressTracker;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Currency;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @InitiatingFlow
 @StartableByRPC
 public class wRDCentralInitFlow extends FlowLogic<SignedTransaction> {
-    
+
     private final Amount<Currency> initialAmount;
-    
+
     private final ProgressTracker.Step GENERATING_TRANSACTION = new ProgressTracker.Step("Generating transaction based on new wRD Account.");
     private final ProgressTracker.Step VERIFYING_TRANSACTION = new ProgressTracker.Step("Verifying contract constraints.");
     private final ProgressTracker.Step SIGNING_TRANSACTION = new ProgressTracker.Step("Signing transaction with our private key.");
     private final ProgressTracker.Step GATHERING_SIGS = new ProgressTracker.Step("Gathering the counterparty's signature.") {
         @Override
+        @NotNull
         public ProgressTracker childProgressTracker() {
             return CollectSignaturesFlow.Companion.tracker();
         }
     };
-    private final ProgressTracker.Step FINALISING_TRANSACTION = new ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+    private final ProgressTracker.Step FINALISING_TRANSACTION = new ProgressTracker.Step("Obtaining notary signature" +
+            " and recording transaction.") {
         @Override
+        @NotNull
         public ProgressTracker childProgressTracker() {
             return FinalityFlow.Companion.tracker();
         }
     };
+    // TODO: Add tracker for observer report flow, ensure enable companion tracker first.
 
     private final ProgressTracker progressTracker = new ProgressTracker(
             GENERATING_TRANSACTION,
@@ -58,50 +64,68 @@ public class wRDCentralInitFlow extends FlowLogic<SignedTransaction> {
     @Suspendable
     @Override
     public SignedTransaction call() throws FlowException {
-        final Party notary = getServiceHub().getNetworkMapCache().getNotary(CordaX500Name.parse("O=Notary,L=Jakarta,C=ID"));
+        // 1. Define all parties involved
+        final Party notary = getServiceHub().getNetworkMapCache().getNotary(CordaX500Name.parse("O=Notary,L=Jakarta," +
+                "C=ID"));
         final Party kdr = getOurIdentity();
 
+        // 2. Generating transaction blueprint
         progressTracker.setCurrentStep(GENERATING_TRANSACTION);
-        
+        // 2.1 Define output state, this flow only use one output state without input state
         final wRDAccountState outputState = new wRDAccountState(kdr, kdr, "IDR", initialAmount);
+        final TransactionBuilder txBuilder;
+        if (notary != null) {
+            txBuilder = new TransactionBuilder(notary)
+                    .addOutputState(outputState, wRDContract.ID)
+                    .addCommand(new wRDContract.Commands.wRDCentralInitCommand(), Arrays.asList(kdr.getOwningKey()));
+        } else {
+            throw new IllegalArgumentException("Notary should be not null.");
+        }
 
-        final TransactionBuilder txBuilder = new TransactionBuilder(notary)
-                .addOutputState(outputState, wRDContract.ID)
-                .addCommand(new wRDContract.Commands.wRDCentralInitCommand(), Arrays.asList(kdr.getOwningKey()));
-
+        // 3. Verify the transaction based on wRD Contract verify method (in current node)
         progressTracker.setCurrentStep(VERIFYING_TRANSACTION);
         txBuilder.verify(getServiceHub());
 
+        // 4. Sign the transaction (in current node)
         progressTracker.setCurrentStep(SIGNING_TRANSACTION);
         final SignedTransaction partSignedTx = getServiceHub().signInitialTransaction(txBuilder);
 
+        // 5. Collect all the required signatures from other nodes
         progressTracker.setCurrentStep(GATHERING_SIGS);
-        List<Party> otherParties = outputState.getParticipants().stream()
-                .map(el -> (Party) el)
-                .distinct() // Add this line to remove duplicates
-                .filter(party -> !party.equals(getOurIdentity())) // Use filter instead of remove for better stream integration
-                .collect(Collectors.toList());
+        List<FlowSession> sessions = new ArrayList<>();
+        // Since outputState.getParticipants() for this flow consist of kdr node only and already signed before
+        // then no need to re-sign, and we can skip this step safely.
 
-        List<FlowSession> sessions = otherParties.stream().map(el -> initiateFlow(el)).collect(Collectors.toList());
-
-        SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(partSignedTx, sessions, GATHERING_SIGS.childProgressTracker()));
-
+        // 6. Return the output of the FinalityFlow which sends the transaction to the notary for verification and
+        // the causes it to be persisted to the vault of appropriate nodes.
         progressTracker.setCurrentStep(FINALISING_TRANSACTION);
-//        Note that in cases where states are being written to only a single party’s ledger and
-//        there’s no counterparty, a notary does not need to be involved and the FinalityFlow
-//        step can be skipped.
-        SignedTransaction finalTx = subFlow(new FinalityFlow(fullySignedTx, sessions, FINALISING_TRANSACTION.childProgressTracker()));
-        
-        // Report to Observer for AML monitoring
+        // Caveat: Note that in cases where states are being written to only a single party’s ledger and there’s no
+        // counterparty, a notary does not need to be involved and the FinalityFlow step can be skipped.
+        // However, calling FinalityFlow when there’s only one party involved in a state will result in a log message.
+         SignedTransaction finalTx = subFlow(new FinalityFlow(partSignedTx, sessions,
+                FINALISING_TRANSACTION.childProgressTracker()));
+
+        // 7. Report to Observer for AML monitoring
         subFlow(new ReportToObserverFlow(finalTx));
-        
+
         return finalTx;
     }
 }
 
+// Note: This flow responder prepared but only used when there are multiple KDR nodes later.
+// Currently with single KDR node, it will be ignored.
 @InitiatedBy(wRDCentralInitFlow.class)
-class wRDCentralInitFlowResponder extends FlowLogic<Void> {
+class wRDCentralInitFlowResponder extends FlowLogic<SignedTransaction> {
     private final FlowSession counterpartySession;
+    private SecureHash txRespSignedId;
+
+    private final ProgressTracker.Step RESP_GET_SIGN = new ProgressTracker.Step("Gathering responder sign tx.") {
+        @Override
+        @NotNull
+        public ProgressTracker childProgressTracker() {
+            return SignTransactionFlow.Companion.tracker();
+        }
+    };
 
     public wRDCentralInitFlowResponder(FlowSession counterpartySession) {
         this.counterpartySession = counterpartySession;
@@ -109,15 +133,26 @@ class wRDCentralInitFlowResponder extends FlowLogic<Void> {
 
     @Suspendable
     @Override
-    public Void call() throws FlowException {
-        SignedTransaction signedTransaction = subFlow(new SignTransactionFlow(counterpartySession) {
-            @Suspendable
-            @Override
-            protected void checkTransaction(SignedTransaction stx) throws FlowException {
-                
+    public SignedTransaction call() throws FlowException {
+        class SignTxFlow extends SignTransactionFlow {
+            private SignTxFlow(FlowSession counterPartySession, ProgressTracker progressTracker) {
+                super(counterPartySession, progressTracker);
             }
-        });
-        subFlow(new ReceiveFinalityFlow(counterpartySession, signedTransaction.getId()));
-        return null;
+
+            @Override
+            protected void checkTransaction(@NotNull SignedTransaction stx) {
+                // Add verify steps here
+                txRespSignedId = stx.getId();
+            }
+        }
+
+        // Create a sign transaction flow
+        SignTxFlow signTxFlow = new SignTxFlow(counterpartySession, RESP_GET_SIGN.childProgressTracker());
+
+        // Run the sign transaction flows to sign the transaction
+        subFlow(signTxFlow);
+
+        // Run the ReceiveFinalityFlow to finalize the transaction and persist it to the vault.
+        return subFlow(new ReceiveFinalityFlow(counterpartySession, txRespSignedId));
     }
 }
