c0813f242c594c8b874d30537073e7ab753e9247
reevald <mochammadgalangrivaldo@gmail.com>
2025-08-02 22:16:46 +0700

    Refactor states to address different version in different node

--- DLT/contracts/src/main/java/com/trace/states/rRDAccountState.java
+++ DLT/contracts/src/main/java/com/trace/states/rRDAccountState.java
@@ -8,7 +8,9 @@ import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
 import net.corda.core.serialization.ConstructorForDeserialization;
+import org.jetbrains.annotations.NotNull;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Currency;
 import java.util.List;
@@ -16,53 +18,80 @@ import java.util.List;
 @BelongsToContract(rRDContract.class)
 public class rRDAccountState implements LinearState {
     private final UniqueIdentifier walletId;
+    private final String walletType;
     private final String owner;
+    private final Party ownerWholesaler;
     private final String issuer;
+    private final Party issuerWholesaler;
     private final String tokenType;
     private final Amount<Currency> tokenBalance;
-    private final Party wholesalerParty;
+    private final long version;
+    private final Instant lastModified;
 
     @ConstructorForDeserialization
-    public rRDAccountState(UniqueIdentifier walletId, String owner, String issuer, String tokenType, 
-                          Amount<Currency> tokenBalance, Party wholesalerParty) {
+    public rRDAccountState(UniqueIdentifier walletId, String walletType, String owner, Party ownerWholesaler,
+                           String issuer, Party issuerWholesaler, String tokenType, Amount<Currency> tokenBalance,
+                           long version, Instant lastModified) {
         this.walletId = walletId;
+        this.walletType = walletType;
         this.owner = owner;
+        this.ownerWholesaler = ownerWholesaler;
         this.issuer = issuer;
+        this.issuerWholesaler = issuerWholesaler;
         this.tokenType = tokenType;
         this.tokenBalance = tokenBalance;
-        this.wholesalerParty = wholesalerParty;
+        this.version = version;
+        this.lastModified = lastModified;
     }
 
-    public rRDAccountState(String owner, String issuer, String tokenType, 
-                          Amount<Currency> tokenBalance, Party wholesalerParty) {
+    public rRDAccountState(String walletType, String owner, Party ownerWholesaler, String issuer,
+                           Party issuerWholesaler, String tokenType, Amount<Currency> tokenBalance) {
         this.walletId = new UniqueIdentifier();
+        this.walletType = walletType;
         this.owner = owner;
+        this.ownerWholesaler = ownerWholesaler;
         this.issuer = issuer;
+        this.issuerWholesaler = issuerWholesaler;
         this.tokenType = tokenType;
         this.tokenBalance = tokenBalance;
-        this.wholesalerParty = wholesalerParty;
+        this.version = 1L;
+        this.lastModified = Instant.now();
     }
 
+    @NotNull
     @Override
     public UniqueIdentifier getLinearId() {
         return walletId;
     }
 
+    @NotNull
     @Override
     public List<AbstractParty> getParticipants() {
-        return Arrays.asList(wholesalerParty);
+        return Arrays.asList(ownerWholesaler, issuerWholesaler);
     }
 
     public UniqueIdentifier getWalletId() {
         return walletId;
     }
 
+    public String getWalletType() {
+        return walletType;
+    }
+
     public String getOwner() {
         return owner;
     }
 
+    public Party getOwnerWholesaler() {
+        return ownerWholesaler;
+    }
+
     public String getIssuer() {
         return issuer;
+    }
+
+    public Party getIssuerWholesaler() {
+        return issuerWholesaler;
     }
 
     public String getTokenType() {
@@ -73,26 +102,24 @@ public class rRDAccountState implements LinearState {
         return tokenBalance;
     }
 
-    public Party getWholesalerParty() {
-        return wholesalerParty;
+    public long getVersion() {
+        return version;
     }
 
-    public rRDAccountState withNewBalance(Amount<Currency> newBalance) {
-        return new rRDAccountState(this.walletId, this.owner, this.issuer, this.tokenType, newBalance, this.wholesalerParty);
+    public Instant getLastModified() {
+        return lastModified;
     }
 
-    public String getWholesalerFromOwner() {
-        if (owner.contains("-")) {
-            return owner.split("-")[0];
-        }
-        return owner;
+    public rRDAccountState withNewBalanceAndIssuer(Amount<Currency> newBalance, String issuer, Party issuerWholesaler) {
+        return new rRDAccountState(this.walletId, this.walletType, this.owner, this.ownerWholesaler, issuer,
+                issuerWholesaler, this.tokenType, newBalance, this.version + 1, Instant.now());
     }
 
-    public boolean isPeritel() {
-        return owner.contains("-P-");
+    public boolean isRetailer() {
+        return walletType.equals("RETAILER");
     }
 
     public boolean isRetail() {
-        return owner.contains("-R-");
+        return walletType.equals("RETAIL");
     }
 }
--- DLT/contracts/src/main/java/com/trace/states/wRDAccountState.java
+++ DLT/contracts/src/main/java/com/trace/states/wRDAccountState.java
@@ -1,6 +1,5 @@
 package com.trace.states;
 
-import com.trace.contracts.rRDContract;
 import com.trace.contracts.wRDContract;
 import net.corda.core.contracts.Amount;
 import net.corda.core.contracts.BelongsToContract;
@@ -9,7 +8,9 @@ import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
 import net.corda.core.serialization.ConstructorForDeserialization;
+import org.jetbrains.annotations.NotNull;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Currency;
 import java.util.List;
@@ -21,14 +22,19 @@ public class wRDAccountState implements LinearState {
     private final Party issuer;
     private final String tokenType;
     private final Amount<Currency> tokenBalance;
+    private final long version;
+    private final Instant lastModified;
 
     @ConstructorForDeserialization
-    public wRDAccountState(UniqueIdentifier walletId, Party owner, Party issuer, String tokenType, Amount<Currency> tokenBalance) {
+    public wRDAccountState(UniqueIdentifier walletId, Party owner, Party issuer, String tokenType,
+                           Amount<Currency> tokenBalance, long version, Instant lastModified) {
         this.walletId = walletId;
         this.owner = owner;
         this.issuer = issuer;
         this.tokenType = tokenType;
         this.tokenBalance = tokenBalance;
+        this.version = version;
+        this.lastModified = lastModified;
     }
 
     public wRDAccountState(Party owner, Party issuer, String tokenType, Amount<Currency> tokenBalance) {
@@ -37,13 +43,17 @@ public class wRDAccountState implements LinearState {
         this.issuer = issuer;
         this.tokenType = tokenType;
         this.tokenBalance = tokenBalance;
+        this.version = 1L;
+        this.lastModified = Instant.now();
     }
 
+    @NotNull
     @Override
     public UniqueIdentifier getLinearId() {
         return walletId;
     }
 
+    @NotNull
     @Override
     public List<AbstractParty> getParticipants() {
         return Arrays.asList(owner, issuer);
@@ -69,7 +79,16 @@ public class wRDAccountState implements LinearState {
         return tokenBalance;
     }
 
-    public wRDAccountState withNewBalance(Amount<Currency> newBalance) {
-        return new wRDAccountState(this.walletId, this.owner, this.issuer, this.tokenType, newBalance);
+    public long getVersion() {
+        return version;
+    }
+
+    public Instant getLastModified() {
+        return lastModified;
+    }
+
+    public wRDAccountState withNewBalanceAndIssuer(Amount<Currency> newBalance, Party newIssuer) {
+        return new wRDAccountState(this.walletId, this.owner, newIssuer, this.tokenType, newBalance, this.version + 1,
+                Instant.now());
     }
 }
